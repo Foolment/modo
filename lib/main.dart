4c5be474097d397ac7d8289317c810378ed555ce
kelvinmo <kelvinmo@tencent.com>
2020-02-08 22:38:39 +0800

    add demo text

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:Modo/widget/task-list-page.dart';
+import 'package:Modo/widget/todo-list-page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() => runApp(ModoApp());
 
 class ModoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // debugPaintSizeEnabled = true;
     return MaterialApp(
       home: Scaffold(body: TaskListPage()),
     );
